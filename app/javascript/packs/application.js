ef410bcb10463d0968ed1288ee2d7e06a09eacc8
Tepen <elcabron.penyah@gmail.com>
2021-06-20 16:54:51 +0800

    add Flatpickr - date picker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,14 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import "controllers"
+
+import flatpickr from "flatpickr"
+require("flatpickr/dist/flatpickr.css")
+
+document.addEventListener("turbolinks:load", () => {
+  flatpickr("[data-behavior='flatpickr']", {
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d"
+  })
+})
