65ffea00eb2da72e883022d641a5d9792faf12a7
Tepen <elcabron.penyah@gmail.com>
2021-08-12 11:47:41 +0800

    route for coming_soon page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
 
-  root to: 'posts#index'
+  root to: 'test_javascripts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/intersection_observers', to: 'test_javascripts#intersection_obs', as: 'intersection'
+  get '/coming_soon', to: 'test_javascripts#coming_soon', as: 'coming_soon'
 end
