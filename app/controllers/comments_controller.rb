9baf4076868d3cf374245fbecbd9ee2b2d5b0d73
Tepen <elcabron.penyah@gmail.com>
2021-10-21 16:17:02 +0800

    add create & destroy methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,42 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_comment, only[:create, :destroy]
+  before_action :set_meeting
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = @meetings.comments.create(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+
+    respond_to do |format|
+      format.html { redirect_to @meeting }
+      format.js
+    end
+  end
+
+  def destroy
+    @comment = @meetings.comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to @meeting
+  end
+
+  private
+  def set_meeting
+    @meeting = Meeting.find(params[:meeting_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+  
+  def comment_params
+    params
+    .require(:comment)
+    .permit(:body)
+  end
 end
