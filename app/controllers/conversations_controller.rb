2d9bac4d79fa552a8429c33d73e9825ca207b207
Tepen <elcabron.penyah@gmail.com>
2021-10-26 20:26:54 +0800

    add show for conversations

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,4 +3,8 @@ class ConversationsController < ApplicationController
   def index
     @conversations = current_user.mailbox.conversations
   end
+
+  def show
+    @conversation = current_user.mailbox.conversations.find(params[:id])
+  end
 end
