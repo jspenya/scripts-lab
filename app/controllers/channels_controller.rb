1dcf7b8f76c16002eae77de73d0ba137db569186
Tepen <elcabron.penyah@gmail.com>
2021-10-29 00:00:14 +0800

    add authenticate user

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,4 +1,5 @@
 class ChannelsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_channel, only: %i[ show edit update destroy ]
 
   # GET /channels or /channels.json
